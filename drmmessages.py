593e63cec0600c2129566c60e194059167822173
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-06-19 18:10:43 -0500

    se acordó el while usando el módulo para saber cuanto queda de restante, el cual es el mínimo restante después de dar x vueltas

--- drmmessages.py
+++ drmmessages.py
@@ -8,15 +8,18 @@ def rotation(str):
 def forward(str, positions):
     str_new = []
     for char in str:
-        char_new = ord(char) - 65 + positions
+        # char_new = ord(char) - 65 + positions
+        char_new = ord(char) - 65 + positions % 26
         if char_new <= 25:
             str_new.append(chr(char_new + 65))
         else:
+
             while char_new > 25:
                 char_new -= 26
             str_new.append(chr(char_new + 65))
 
     return ''.join(str_new)
+
 
 def merge(first, second):
     out = []
